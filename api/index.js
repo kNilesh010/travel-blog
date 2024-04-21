9646cf66c8502a76329f0524f5d2bd9639262ac2
Nilesh <91608463+kNilesh010@users.noreply.github.com>
2024-04-21 10:23:53 +0530

    Created the signup api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import { fail } from "assert";
 dotenv.config();
 
 mongoose
@@ -11,7 +14,22 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("server is running on port 3000!");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const errorCode = err.statusCode || 500;
+  const errorMessage = err.message || "Internal server error";
+  res.status(errorCode).json({
+    success: false,
+    errorCode,
+    errorMessage,
+  });
 });
